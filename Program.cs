067d067d33f9565a5160e6b9dc5d54976e06415f
OlivierMiracle <adam.cisz@pm.me>
2022-03-20 20:21:55 +0100

    Initial commit
    Initial commit of a simple terminal flashcard game for learning stuff

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Spectre.Console;
+
+namespace flashcard;
+
+public static class Flashcard
+{
+    public static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Hello World![/]");
+    }
+}
